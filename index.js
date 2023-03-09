df26a69fa9fe140808f0e1b96c3a53ab883b9e72
Jose Sequeira <jsc190@gmail.com>
2023-03-09 17:01:36 -0600

    mongoose

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require("cors");
+const path = require('path');
 
+const app = express();
+const port = 3000;
+
+app.use(cors());
+
+const conn = mongoose.createConnection("mongodb://localhost:27017/test");
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
-  })
+    res.send('Hello World!');
+});
   
-  app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-  })
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
+
+app.get("/user/login", function(req, res){
+    res.sendFile();
+});
