29753c7d26675c259292d92e99178342a770539d
Jose Sequeira <jsc190@gmail.com>
2023-03-11 16:03:13 -0600

    post routes, middleware

--- app.js
+++ app.js
@@ -1,27 +1,26 @@
 require("dotenv").config();
 require("./configs/database");
 const controller = require('./controllers/controller');
-
+const auth = require('./middleware/auth');
 const express = require('express');
 const cors = require('cors');
-const path = require('path');
 const bodyparser = require('body-parser');
-
-const User = require("./models/User");
 
 const app = express();
 
 app.use(cors());
 app.use(bodyparser.urlencoded({extended:false}));
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+app.get('/', controller.loginView);
   
 app.get("/login", controller.loginView);
 
 app.get("/register", controller.registerView);
 
-app.post("/register")
+app.get("/account", auth, controller.accountView);
+
+app.post('/login', controller.loginUser);
+
+app.post("/register", controller.registerUser);
 
 module.exports = app;
--- controllers/controller.js
+++ controllers/controller.js
@@ -4,24 +4,28 @@ const jwt = require('jsonwebtoken');
 const path = require('path');
 
 const registerView = (req, res) => {
-    res.render("register", {
-    } );
+    /*res.render("register", {
+    } );*/
+
+    res.sendFile(path.join(__dirname, './../views/register.html'));
 };
 
 const loginView = (req, res) => {
-
     /*res.render("login", {
-        
     } );*/
 
     res.sendFile(path.join(__dirname, './../views/login.html'));
 };
 
+const accountView = (req, res) => {
+    res.sendFile(path.join(__dirname, './../views/account.html'));
+}
+
 const registerUser = async (req, res) => {
     let { email, firstName, lastName, username, password, discord, phone, gender, birthDate, identification, 
         academicInstitution, shirtSize, medicalConditions, dietaryConditions, hasParticipated, knowledge } = req.body;
     
-    if (!email || !firstName || !lastName || !username || !password || !discord || !phone || !gender || !birthDate || !academicInstitution) {
+    if (!(email && firstName && lastName && username && password && discord && phone && gender && birthDate && academicInstitution)) {
         throw new Error('You must fill all required fields!');
     }
 
@@ -36,13 +40,15 @@ const registerUser = async (req, res) => {
         if (err) throw err;
 
         const token = jwt.sign(
-            { user: email, username },
+            { 
+                user: email,
+                exp: Math.floor(Date.now() / 1000) + (60*60*24*7),
+            },
             process.env.TOKEN_KEY,
-
         );
 
         const user = new User({
-            email, 
+            _id: email, 
             firstName, 
             lastName, 
             username, 
@@ -58,14 +64,40 @@ const registerUser = async (req, res) => {
             dietaryConditions, 
             hasParticipated, 
             knowledge,
+            token,
         });
 
         user.save()
-            .then(res.redirect())
+            .then(res.redirect("/account"));
     });
+};
+
+const loginUser = async (req, res) => {
+    const { email, password } = req.body;
+
+    if (!(email && password)) {
+        throw new Error('You must fill all required fields!');
+    }
+
+    const user = await User.findById(email);
+
+    if (user && (bcrypt.compare(password, user.password))) {
+        const token = jwt.sign(
+            { 
+                user: email,
+                exp: Math.floor(Date.now() / 1000) + (60*60*24*7),
+            },
+            process.env.TOKEN_KEY,
+        );
+        
+        user.token = token;
+    }
 };
 
 module.exports =  {
     registerView,
-    loginView
+    loginView,
+    accountView,
+    registerUser,
+    loginUser,
 };
